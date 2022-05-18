2312f29e24d424bb4e54fb2fbcb9beff2a3d56fa
Franpanozzo <francisco.panozzosf@gmail.com>
2022-05-18 15:22:39 -0300

    first push

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+
+require('dotenv').config();
+
+const { loadTeamsData } = require('./models/teams.model');
+const { loadPlayersData } = require('./models/players.model');
+
+const app = require('./app');
+const { mongoConnect } = require('./services/mongo');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+async function startServer() {
+  await mongoConnect(process.env.MONGO_URL);
+
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  })
+}
+
+startServer();
