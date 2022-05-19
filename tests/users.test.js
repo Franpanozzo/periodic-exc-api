e7d1e4ba5d9f69201834d93b603656227eafc693
Franpanozzo <francisco.panozzosf@gmail.com>
2022-05-19 14:49:37 -0300

    fix test structure

--- tests/users.test.js
+++ tests/users.test.js
@@ -11,11 +11,12 @@ const {
 
 jest.setTimeout(1000000);
 
-describe('Users API', () => {
-  beforeAll(async () => {
+describe('Periodic Users API', () => {
+
+  beforeAll( async () => {
     await mongoConnect(process.env.MONGO_TEST_URL);
     await loadUsersData();
-  });
+  })
 
   afterAll(async () => {
     await mongoDisconnect();
@@ -23,13 +24,14 @@ describe('Users API', () => {
 
 
   describe('Login', () => {
+
     test('tryning to do a request without logging in should respond 401', async () => {
       const response = await request(app)
       .get('/v1/users')
       .expect('Content-Type', "text/plain; charset=utf-8")
       .expect(401); 
     })
-
+  
     test('tryning to do a request logged should should respond 200', async () => {
       const response = await request(app)
       .post('/v1/login')
@@ -38,7 +40,7 @@ describe('Users API', () => {
       })
       .expect('Content-Type', /json/)
       .expect(200); 
-
+  
       const accessToken = response.body.accessToken;
       console.log(`Access token is: ${accessToken}`);
   
@@ -48,10 +50,10 @@ describe('Users API', () => {
         .expect('Content-Type', /json/)
         .expect(200); 
     })
-
+  
   })
-
-  describe('GET /users', () => {
+  
+  describe('Users API', () => {
     let accessToken;
     beforeAll(async () => {
       const response = await request(app)
@@ -61,30 +63,34 @@ describe('Users API', () => {
       })
       .expect('Content-Type', /json/)
       .expect(200); 
-
+  
       accessToken = response.body.accessToken;
-    })
-
-
-    test('GET /users should respond status 200', async () => {
-      const response = await request(app)
-      .get('/v1/users')
-      .set('Authorization', `Bearer ${accessToken}`)
-      .expect('Content-Type', /json/)
-      .expect(200); 
-    })
-
-    test('GET /users with the email as param should return 200 with the expected user', async () => {
-      const response = await request(app)
-      .get('/v1/users/francisco.panozzosf@gmail.com')
-      .set('Authorization', `Bearer ${accessToken}`)
-      .expect('Content-Type', /json/)
-      .expect(200); 
-
-      const player = response.body; 
-      expect(player.last_name).toBe('Panozzo');
+    });
+  
+    describe('GET /users', () => {
+  
+      test('GET /users should respond status 200', async () => {
+        const response = await request(app)
+        .get('/v1/users')
+        .set('Authorization', `Bearer ${accessToken}`)
+        .expect('Content-Type', /json/)
+        .expect(200); 
+      })
+  
+      test('GET /users with the email as param should return 200 with the expected user', async () => {
+        const response = await request(app)
+        .get('/v1/users/francisco.panozzosf@gmail.com')
+        .set('Authorization', `Bearer ${accessToken}`)
+        .expect('Content-Type', /json/)
+        .expect(200); 
+  
+        const player = response.body; 
+        expect(player.last_name).toBe('Panozzo');
+      })
+    
     })
   
   })
-
+  
 })
+
