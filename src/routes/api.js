05eb67658027b58ca1b2b83af882bfd0e58ffc54
Franpanozzo <francisco.panozzosf@gmail.com>
2022-05-20 06:28:26 -0300

    reduce token expiration time

--- src/routes/api.js
+++ src/routes/api.js
@@ -9,7 +9,7 @@ const api = express.Router();
 api.post('/login', (req, res) => {
   const username = req.body.username; //Podría tmb sacar la contraseña ds.
   const user = { name: username }; 
-  const token = jwt.sign({ user }, process.env.SECRET_KEY, { expiresIn: '3h'});
+  const token = jwt.sign({ user }, process.env.SECRET_KEY, { expiresIn: '10m'});
 
   return res.json({
       accessToken: token
